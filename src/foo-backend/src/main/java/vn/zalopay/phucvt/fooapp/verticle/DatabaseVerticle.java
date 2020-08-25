57e7f6a7843ef840a8b363dab50f3e9caf1a8bee
Noradomi <fakemsi1999@gmail.com>
2020-08-26 00:33:56 +0700

    Configuation file yaml and connection to databases, cache

--- src/foo-backend/src/main/java/vn/zalopay/phucvt/fooapp/verticle/DatabaseVerticle.java
+++ src/foo-backend/src/main/java/vn/zalopay/phucvt/fooapp/verticle/DatabaseVerticle.java
@@ -3,6 +3,7 @@ package vn.zalopay.phucvt.fooapp.verticle;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLConnection;
 import io.vertx.mysqlclient.MySQLPool;
@@ -10,22 +11,32 @@ import io.vertx.sqlclient.PoolOptions;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.SqlConnection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class DatabaseVerticle extends AbstractVerticle {
 
-    public static void main(String[] args) {
-        Vertx vertx= Vertx.vertx();
+    private static final Logger LOGGER = LogManager.getLogger(DatabaseVerticle.class);
 
+    private JsonObject config;
+
+    public DatabaseVerticle(JsonObject config){
+        this.config = config;
     }
 
     @Override
     public void start() throws Exception {
+        LOGGER.info("Connection to MySql server");
+
         MySQLConnectOptions connectOptions = new MySQLConnectOptions()
-                .setPort(3306)
-                .setHost("localhost")
-                .setDatabase("fooapp")
-                .setUser("root")
-                .setPassword("1234");
+                .setPort(config.getInteger("port",3306))
+                .setHost(config.getString("host","localhost"))
+                .setDatabase(config.getString("database","fooapp"))
+                .setUser(config.getString("user","root"))
+                .setPassword(config.getString("password","1234"))
+                .setCharset("utf8mb4")
+                .setCollation("utf8mb4_unicode_ci");
 
         PoolOptions poolOptions = new PoolOptions().setMaxSize(5);
 
@@ -36,7 +47,7 @@ public class DatabaseVerticle extends AbstractVerticle {
                 System.out.println("Connected");
                 SqlConnection conn = ar.result();
                 conn
-                        .query("SELECT * FROM user")
+                        .query("SHOW DATABASES;")
                         .execute(ar2 -> {
                             if (ar2.succeeded()) {
                                 RowSet<Row> result = ar2.result();
