eecfe7e2b4a5898fc1138e4940996aa67d76e42f
Noradomi <fakemsi1999@gmail.com>
2020-09-01 16:29:13 +0700

    Create init-websocket using Sokette

--- src/foo-frontend/src/services/chat-single.js
+++ src/foo-frontend/src/services/chat-single.js
@@ -1,55 +1,24 @@
-import { ws_host } from './api';
+
 import store from '../redux/fooStore';
-import { webSocketConnected, login} from '../redux/fooAction';
-import { getUserIdFromStorage, getJwtFromStorage } from '../utils/utils';
+import { webSocketConnected} from '../redux/fooAction';
+import { getUserIdFromStorage } from '../utils/utils';
+import {initialWebSocket} from './init-websocket'
 
-let ws = null;
+// let ws = null;
 
-export function wsConnect(msg) {
-	// Current user id
-	let senderId = Number(getUserIdFromStorage());
+export function wsConnect() {
+    let webSocket = initialWebSocket();
 
-	// Return websocket promise
-	if (ws === null) {
-		let token = getJwtFromStorage();
-		let webSocket = new WebSocket(ws_host +"chat"+`?token=${token}`);
+    let send = function(receiverId, message) {
+        let sentMessage = {
+            receiver_id: receiverId,
+            msg: message,
+            type: 'SEND'
+        };
+        // webSocket.send(JSON.stringify(sentMessage));
+        webSocket.json(sentMessage);
+    };
 
-		// When connect successful
-		webSocket.onopen = (event) => {
-            console.log("Created new web socket connection");
+    store.dispatch(webSocketConnected(webSocket, send));
 
-			ws = webSocket;
-
-			// Function to send chat message
-			let send = function(receiverId, message) {
-				let sentMessage = {
-					receiver_id: receiverId,
-					msg: message,
-					type: 'SEND'
-				};
-				webSocket.send(JSON.stringify(sentMessage));
-			};
-
-			// Test message
-			send(senderId, msg);
-
-			// Notify to redux
-			store.dispatch(webSocketConnected(webSocket, send));
-
-			// // Handle chat message
-			// webSocket.onmessage = (messageEvent) => {
-			// 	let jsonMessage = JSON.parse(messageEvent.data);
-			// 	if (jsonMessage.type === 'CHAT') {
-			// 		jsonMessage.isMine = jsonMessage.senderId === senderId;
-			// 		store.dispatch(receiveMessage(jsonMessage));
-			// 	} else if (jsonMessage.type === 'SEND_BACK') {
-			// 		if (jsonMessage.receiverId === senderId) {
-			// 			return;
-			// 		}
-			// 		jsonMessage.isMine = true;
-			// 		store.dispatch(sendbackMessage(jsonMessage));
-			// 	}
-			// };
-		};
-	}
 }
